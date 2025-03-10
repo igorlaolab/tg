8dcef58251ca33a09cb0a8cdee8f8ce0ef2f05f6
samartsevigor <igorsamartsev@gmail.com>
2025-03-11 01:11:07 +0600

    Updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
-import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
 
 import { Root } from '@/components/Root.tsx';
 import { EnvUnsupported } from '@/components/EnvUnsupported.tsx';
@@ -16,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 try {
   // Configure all application dependencies.
-  init(retrieveLaunchParams().startParam === 'debug' || import.meta.env.DEV);
+  init(false);
 
   root.render(
     <StrictMode>
