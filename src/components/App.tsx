fb5ad865f0a65de0793b89d1c30a6cc9ee30f61b
samartsevigor <igorsamartsev@gmail.com>
2025-03-13 22:05:59 +0600

    Merge with markup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,23 @@
 import { useLaunchParams, miniApp, useSignal } from '@telegram-apps/sdk-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
 import { Navigate, Route, Routes, BrowserRouter } from 'react-router-dom';
+import {useEffect} from 'react'
 
 import { routes } from '@/navigation/routes.tsx';
 
 export function App() {
   const lp = useLaunchParams();
   const isDark = useSignal(miniApp.isDark);
+
+  useEffect(() => {
+    if (
+      window.Telegram &&
+      window.Telegram.WebApp &&
+      typeof window.Telegram.WebApp.setupSwipeBehavior === 'function'
+    ) {
+      window.Telegram.WebApp.setupSwipeBehavior({ swipe_enabled: false });
+    }
+  }, []);
 
   return (
     <AppRoot
