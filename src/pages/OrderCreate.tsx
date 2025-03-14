f3ca4a8d775681dca4fc35fda4a55d6a9e72c82c
samartsevigor <igorsamartsev@gmail.com>
2025-03-14 16:37:34 +0600

    Merge with markup

--- src/pages/OrderCreate.tsx
+++ src/pages/OrderCreate.tsx
@@ -77,7 +77,7 @@ export const OrderCreate: React.FC = () => {
 
   return (
     <Page>
-      <Stack justifyContent="space-between" sx={{ backgroundColor: 'red', flex: 1 }}>
+      <Stack justifyContent="space-between" sx={{ flex: 1 }}>
         <ContentContainer sx={{ mt: 3 }}>
           <Segmented
             segments={[
