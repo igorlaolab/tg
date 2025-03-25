ef1983ca25ff796f77f7e392481f840db4c2b91c
samartsevigor <igorsamartsev@gmail.com>
2025-03-25 17:16:32 +0600

    Added AppRoot wrapper

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,19 @@
 import { Navigate, Route, Routes, BrowserRouter } from 'react-router-dom'
 
 import { routes } from '@/navigation/routes.tsx';
+import {AppRoot} from "@telegram-apps/telegram-ui";
 
 export function App() {
   return (
+    <AppRoot
+      appearance='dark'
+    >
       <BrowserRouter>
         <Routes>
           {routes.map((route) => <Route key={route.path} {...route} />)}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
+    </AppRoot>
   );
 }
